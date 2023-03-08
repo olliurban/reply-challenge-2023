73df2e230572193ab2e32804d2eded32ee1a484e
olliurban <oliver.urbaniak@web.de>
2023-03-08 23:12:01 +0100

    Added IO helper, nothing special here

--- src/io/ProblemReader.java
+++ src/io/ProblemReader.java
@@ -2,10 +2,22 @@ package io;
 
 import entities.ProblemInstance;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class ProblemReader {
 
     public ProblemInstance readerFile(String path) {
-        //TODO: impl
-        return null;
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(path));
+
+            //TODO: impl
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        return null; // TODO return proper problem instance
     }
 }
--- src/io/SolutionWriter.java
+++ src/io/SolutionWriter.java
@@ -1,12 +1,26 @@
 package io;
 
-import entities.ProblemInstance;
 import entities.SolutionInstance;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class SolutionWriter {
 
+    public void writeSolution(SolutionInstance solution, String path) {
+        File file = new File(path);
 
-    public void writeSolution(SolutionInstance solution) {
-        //TODO: impl
+        try {
+            PrintWriter printWriter = new PrintWriter(new FileWriter(file, false));
+
+            // TODO: Beam me up, Scotty
+
+            printWriter.flush();
+            printWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
