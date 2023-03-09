e44a2622cf65c1dae55931638053a4e04f7e2837
Kamil Rosiak <k.rosiak@tu-bs.de>
2023-03-09 13:01:14 +0100

    Added Solver Execution

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,6 +1,18 @@
+import entities.ProblemInstance;
+import io.ProblemReader;
+import io.SolutionWriter;
+import solver.ISolver;
+import solver.Solver;
+
 public class MainClass {
 
     public static void main(String[] args) {
         System.out.println("Test");
+
+        ProblemInstance problem = new ProblemReader().readerFile("");
+        ISolver solver = new Solver();
+        SolutionWriter writter = new SolutionWriter();
+        writter.writeSolution(solver.solve(problem),"");
+
     }
 }
---
+++ src/solver/Solver.java
@@ -0,0 +1,12 @@
+package solver;
+
+import entities.ProblemInstance;
+import entities.SolutionInstance;
+
+public class Solver implements ISolver {
+
+    @Override
+    public SolutionInstance solve(ProblemInstance problem) {
+        return null;
+    }
+}
